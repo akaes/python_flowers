875120c067a77b97af24a83f54cd8e1d657dd86f
Andrea Knabe <andrea.knabe@posteo.de>
2015-07-19 12:29:12 +0200

    flowers
    drawing geometric figures with maths

---
+++ quadrat_turtle_math.py
@@ -0,0 +1,23 @@
+import turtle
+turtle.color('CornflowerBlue', 'CornflowerBlue')
+
+sidelength = 40 #eigenen wert einsetzen
+radius_incircle = sidelength * 1.9626
+
+for a in range(5):
+
+    for m in range(18):
+        turtle.left(20)
+
+        for i in range(4):
+            turtle.forward(sidelength)
+            turtle.right(90)
+
+    turtle.penup()
+    turtle.left(72)
+    turtle.forward(radius_incircle)
+    turtle.pendown()
+
+turtle.exitonclick()
+
+#Liste der Farben: http://www.tcl.tk/man/tcl8.5/TkCmd/colors.htm
